a43a2023fb1c668c038e2a51cecb34155ee47da9
kille <killejuliettefall@esp.sn>
2021-11-04 17:36:09 +0000

    Test choix article

--- Article.java
+++ Article.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Article{
 	private String nom;
 	private int prix;
@@ -41,6 +42,18 @@ public class Article{
 
 	public static void main(String[] args) {
 		Article article1 = new Article("Pantalon ZARA",25000,1000);
+		Article article2 = new Article("Macbook Pro touch bar 2020",800000,100);
+		Article article3 = new Article("Iphone 13",5000000,320);
+
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Que souhaitez vous acheter, Voila nos article et leur prix:  ");
+		System.out.println("nom: " + article1.getNom() + " prix: " + article1.getPrix() + " et les frais de paiement sont de " + article1.getFrais());
+		System.out.println("nom: " + article2.getNom() + " prix: " + article2.getPrix() + " et les frais de paiement sont de " + article2.getFrais());
+		System.out.println("nom: " + article3.getNom() + " prix: " + article3.getPrix() + " et les frais de paiement sont de " + article3.getFrais());
+
+        String article = reader.next();
+
+		//ondition TO DO
 		article1.payer(new PaypalStrategy("mb@gmail.com", "p001"));
 	}
 }
