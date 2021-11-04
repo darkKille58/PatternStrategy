f355e43e13c5ad9679eb9f7b49696f47ccfd986a
cathysadykh <81162530+cathysadykh@users.noreply.github.com>
2021-11-04 14:34:47 +0000

    Add files via upload

---
+++ WaveStrategy.java
@@ -0,0 +1,21 @@
+public class WaveStrategy implements PaiementStrategy {
+    private String numTelephone;
+    private String password;
+    public WaveStrategy(String numero, String pass){
+        this.numTelephone= numero;
+        this.password=pass;
+    }
+    @Override
+    public void payer(int montant, int frais){
+        System.out.println( montant + frais + "CFA  payes par wave. ");
+    }
+
+//test 
+    public static void main(String[] args)
+    {
+        String numero= "777212345";
+        String pass = "1234";
+        WaveStrategy paiement1 = new WaveStrategy(numero,pass);
+        paiement1.payer(150000,2000);
+    }
+}
